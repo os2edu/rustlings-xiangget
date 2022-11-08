6f2f1cf827d72192ea3f82a28390cc693cb9fefb
xiangget <1416524847@qq.com>
2022-11-08 15:29:02 +0800

    update

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,7 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
@@ -40,6 +40,43 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+
+        if !scores.contains_key(&team_1_name){
+            scores.insert(team_1_name.clone(), Team{
+                name: team_1_name.clone(),
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score
+            });
+        }
+        else{
+            if let Some(team1)=scores.get(&team_1_name){
+                let goals_scored = team1.goals_scored + team_1_score;
+                let goals_conceded = team1.goals_conceded + team_2_score;
+                scores.insert(team_1_name.clone(), Team{
+                    name: team_1_name.clone(),
+                    goals_scored,
+                    goals_conceded
+                });
+            }
+            }
+            if !scores.contains_key(&team_2_name) {
+                scores.insert(team_2_name.clone(), Team {
+                    name: team_2_name.clone(),
+                    goals_scored: team_2_score,
+                    goals_conceded: team_1_score
+                });
+            } else {
+                if let Some(team2) = scores.get(&team_2_name) {
+                    let goals_scored = team2.goals_scored + team_2_score;
+                    let goals_conceded = team2.goals_conceded + team_1_score;
+                    scores.insert(team_2_name.clone(), Team{
+                        name: team_2_name.clone(),
+                        goals_scored,
+                        goals_conceded
+                    });
+                }
+            }
+
     }
     scores
 }
