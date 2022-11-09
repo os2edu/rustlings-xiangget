81fe986875daa9f79c3ccc5ca0d77c840a4e904a
xiangget <1416524847@qq.com>
2022-11-09 21:15:28 +0800

    update

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -17,7 +17,7 @@
 // one is a lot shorter!
 // Execute `rustlings hint errors2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 use std::num::ParseIntError;
 
@@ -25,8 +25,12 @@ pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
     let qty = item_quantity.parse::<i32>();
+    match qty{
+        Ok(qty)=>Ok(qty * cost_per_item + processing_fee),
+        Err(qty)=>Err(qty),
+    }
 
-    Ok(qty * cost_per_item + processing_fee)
+    
 }
 
 #[cfg(test)]
